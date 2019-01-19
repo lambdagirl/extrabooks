a5fc483fbddac378ee8ca718ac86ec67077c9c24
Peizhi Zheng <parisa1206@gmail.com>
2019-01-19 12:32:28 -0800

    Heroku congif

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn extrabooks_app.wsgi --log-file -
+web: gunicorn extrabooks_app.wsgi
--- extrabooks_app/settings.py
+++ extrabooks_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,6 @@ EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = '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'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+
+django_heroku.settings(locals())
