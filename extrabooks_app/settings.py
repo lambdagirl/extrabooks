1016fea455776cd42ee643dc96787c59422244c1
Peizhi Zheng <parisa1206@gmail.com>
2019-02-10 20:51:21 -0800

    addhost

--- extrabooks_app/settings.py
+++ extrabooks_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'saxx*8^cib1va1%w70yj5j3oge=pgj%q=ug=qwtp+%-s7_wpn+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['extrabooks.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['extrabooks.com', 'localhost','127.0.0.1','extrabook.herokuapp.com']
 
 
 # Application definition
